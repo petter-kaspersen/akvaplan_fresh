28b298bbce2231cfc44ad9cdca3c5ae245176f4c
Petter Kaspersen <petter@kaspersen.dev>
2023-02-16 09:43:00 +0100

    Update card to use media bank url

--- routes/components/card.tsx
+++ routes/components/card.tsx
@@ -1,4 +1,4 @@
-import { Head, asset } from "$fresh/runtime.ts";
+import { Head } from "$fresh/runtime.ts";
 import { Card } from "../../components/card.tsx";
 import Layout from "../../components/layout.tsx";
 
@@ -9,7 +9,11 @@ export default function CardRoute() {
         <title>Card Testing</title>
       </Head>
       <Card />
-      <Card backgroundImage={asset("/images/card-example.jpg")} />
+      <Card
+        backgroundImage={
+          "https://preview.sdl.no/v2/dam/ssGDst9O7xlTb0J6I2g_Lw/I583h3dwed8?v=1673953218959"
+        }
+      />
     </Layout>
   );
 }
