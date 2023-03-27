7440f7b84a12605e810e628a89dea79a6de35d33
Petter Kaspersen <petter@kaspersen.dev>
2023-03-27 05:59:00 +0200

    Initial start on people page

---
+++ components/people/PeopleScroll.tsx
@@ -0,0 +1,17 @@
+import { Akvaplanist } from "../../services/akvaplanist.ts";
+import HScroll from "../hscroll/HScroll.tsx";
+import { PeopleCard } from "./PeopleCard.tsx";
+
+type Props = {
+  people: Akvaplanist[];
+};
+
+export function PeopleScroll({ people }: Props) {
+  return (
+    <HScroll scrollerId="people-scroll">
+      {people.map((person) => (
+        <PeopleCard person={person} key={person.id} />
+      ))}
+    </HScroll>
+  );
+}
