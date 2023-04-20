8f13dcc56ba53dbaacf1687bebda1d9ed3539ad8
cnrdh <cnrdh@users.noreply.github.com>
2023-04-20 10:57:11 +0200

    fix href

--- services/news_akvaplanists.ts
+++ services/news_akvaplanists.ts
@@ -1,19 +1,15 @@
 import { personURL } from "akvaplan_fresh/services/nav.ts";
 import { t } from "akvaplan_fresh/text/mod.ts";
 
-import {
-  Akvaplanist,
-  News,
-  //NewsMapperArgs,
-} from "akvaplan_fresh/@interfaces/mod.ts";
+import { Akvaplanist, News } from "akvaplan_fresh/@interfaces/mod.ts";
 
 export const newsFromAkvaplanists =
   ({ lang }) =>
-  ({ given, family, created, unit, workplace }: Akvaplanist): News => ({
+  ({ id, given, family, created, unit, workplace }: Akvaplanist): News => ({
     title: `${given} ${family} ${t("people.employed_in")} ${
       t(`unit.${unit}`)
     }, ${workplace}`,
-    //href:
+    href: personURL({ id, given, family, lang }),
     hreflang: lang,
     published: created ?? new Date(),
     type: "person",
