b6cdcf0e6fd49006d7ea4c68752e9fabd64729b5
cnrdh <cnrdh@users.noreply.github.com>
2023-04-25 09:21:10 +0200

    refactor to allow right/left injection

--- components/header/clean_header.tsx
+++ components/header/clean_header.tsx
@@ -1,109 +1,38 @@
-import { ApnLogo, SiteNav } from "akvaplan_fresh/components/mod.ts";
-
-import ButtonOpenDialog from "akvaplan_fresh/islands/button_open_dialog.tsx";
+import { ApnLogo } from "akvaplan_fresh/components/mod.ts";
 
 import { routes } from "akvaplan_fresh/services/nav.ts";
 import { lang as langSignal, t } from "akvaplan_fresh/text/mod.ts";
 
 import borders from "open-props/src/props.borders.js";
+import { SiteMenu } from "./SiteMenu.tsx";
 
-const threeColumnGrid = {
-  margin: 0,
-  padding: borders["--border-size-3"],
-  display: "grid",
-  //background: "var(--surface1)",
-  gridTemplateColumns: "1fr auto 1fr",
-  color: "var(--text1)",
-};
+const Empty = (_ign: unknown) => <span></span>;
+
 export function CleanHeader(
-  { href = "/", lang, HeaderLogo = ApnLogo },
+  {
+    href = "/",
+    lang = langSignal.value,
+    Logo = ApnLogo,
+    Right = SiteMenu,
+    Left = Empty,
+  },
 ) {
   return (
-    <header id="top" style={threeColumnGrid}>
-      <span></span>
+    <header
+      id="top"
+      style={{
+        margin: 0,
+        padding: borders["--border-size-3"],
+        display: "grid",
+        gridTemplateColumns: "1fr auto 1fr",
+        color: "var(--text1)",
+      }}
+    >
+      <Left lang={lang} />
       <a href={href}>
-        <HeaderLogo />
+        <Logo />
       </a>
-      <span
-        class="header-end"
-        style={{
-          display: "grid",
-          justifyContent: "end",
-        }}
-      >
-        <menu>
-          <ButtonOpenDialog data-for="dialog#menu">
-            {lang === "en" ? "Menu" : "Meny"}
-          </ButtonOpenDialog>
-        </menu>
-
-        <dialog
-          id="menu"
-          color-scheme
-          style={{
-            border: "0",
-            background: "var(--surface2)",
-            color: "var(--text1)",
-          }}
-        >
-          <header>
-            <a
-              href="/"
-              aria-label={t("nav.go_home")}
-              style={{ marginTop: "1rem" }}
-            >
-              <ApnLogo />
-            </a>
-          </header>
-
-          <menu
-            style={{
-              margin: 0,
-              //padding: "2rem",
-              background: "var(--surface1)",
-              display: "grid",
-              placeItems: "center",
-              color: "var(--text1)",
-            }}
-          >
-            {
-              /* <form
-              method="get"
-              action={routes(langSignal?.value ?? "no").get("search")}
-              autocomplete="off"
-              style={{
-                display: "grid",
-                gridTemplateColumns: "2fr 1fr",
-                marginTop: "0.5rem",
-                marginBottom: "0.5rem",
-              }}
-            >
-              <input
-                type="search"
-                name="q"
-                value={""}
-                placeholder={t("menu.search.placeholder")}
-              />
-
-              <button type="submit">{t("Search")}</button>
-            </form> */
-            }
-            <SiteNav />
-          </menu>
-          <footer
-            style={{
-              display: "grid",
-              placeItems: "center",
-            }}
-          >
-            <form method="dialog">
-              <button>
-                {lang === "en" ? "Close" : "Lukk"}
-              </button>
-            </form>
-          </footer>
-        </dialog>
-      </span>
+      <Right lang={lang} />
     </header>
   );
 }
