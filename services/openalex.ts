5736718011cc79e7ca4ecb2224d4eb20aac6ee5d
cnrdh <cnrdh@users.noreply.github.com>
2023-04-26 11:52:36 +0200

    Add 404 to debug log

---
+++ services/openalex.ts
@@ -0,0 +1,32 @@
+const base = "https://api.openalex.org";
+const path = "/works";
+const mail = "info@akvaplan.niva.no";
+const endpoint = `${base}${path}`;
+
+const fetchJSON = async (url) => {
+  const r = await fetch(url).catch(() => {});
+  if (r?.ok) {
+    return r.json();
+  } else {
+    throw `GET ${url} failed with status: ${r.status}`;
+  }
+};
+
+export const getOpenAlexWork = async ({ doi, mailto = mail } = {}) =>
+  await fetchJSON(`${endpoint}/doi:${doi}?mailto=${mailto}`);
+
+export const fetchOpenAlexCites = async ({
+  doi,
+  id,
+  per_page = 100,
+  page = 1,
+  mailto = mail,
+} = {}) => {
+  if (!id) {
+    const work = await getOpenAlexWork({ doi });
+    id = work.id;
+  }
+  const url =
+    `${endpoint}?filter=cites:${id}&per_page=${per_page}&page=${page}&mailto=${mailto}`;
+  return fetchJSON(url);
+};
