58837b5ac6ca84d4bea4f01178f39ff530bd06fd
cnrdh <cnrdh@users.noreply.github.com>
2023-04-26 19:38:22 +0200

    refactor from import to fetch

--- services/svc.ts
+++ services/svc.ts
@@ -4,37 +4,42 @@ import { shuffle } from "akvaplan_fresh/grouping/mod.ts";
 
 import { nytek } from "./topic/nytek.tsx";
 
-//Beware, imports are cached
-import services from "https://svc.deno.dev/?x=y2139871298" assert {
-  type: "json",
+const _services = [];
+
+const getServices = async () => {
+  const r = await fetch("https://svc.deno.dev/").catch(() => {});
+  if (r?.ok) {
+    return r.json();
+  }
 };
+
 type Svc = Record<string, string | Number | string[]>;
-export const servicesLevel = (n: Number) =>
-  services.filter(({ level }: Svc) => level === n);
+const servicesLevelFilter = (n: Number) => ({ level }: Svc) => level === n;
 
-const en0 = servicesLevel(0).map((
-  { topic, en, no, details, detaljer, ...s }: Svc,
-) => ({
-  ...s,
-  topic,
-  name: en ?? no,
-  desc: details ?? detaljer,
-  lang: "en",
-  href: serviceGroupURL({ lang: "en", topic }),
-}));
+export const getServicesLevel0 = async (lang: string) => {
+  const svc0 = (await getServices() ?? [])?.filter(servicesLevelFilter(0));
+  const en0 = svc0.map((
+    { topic, en, no, details, detaljer, ...s }: Svc,
+  ) => ({
+    ...s,
+    topic,
+    name: en ?? no,
+    desc: details ?? detaljer,
+    lang: "en",
+    href: serviceGroupURL({ lang: "en", topic }),
+  }));
 
-const no0 = servicesLevel(0).map((
-  { no, en, tema, details, detaljer, ...s }: Svc,
-) => ({
-  ...s,
-  name: no ?? en,
-  desc: detaljer ?? details,
-  topic: tema,
-  lang: "no",
-  href: serviceGroupURL({ lang: "no", topic: tema }),
-}));
+  const no0 = svc0.map((
+    { no, en, tema, details, detaljer, ...s }: Svc,
+  ) => ({
+    ...s,
+    name: no ?? en,
+    desc: detaljer ?? details,
+    topic: tema,
+    lang: "no",
+    href: serviceGroupURL({ lang: "no", topic: tema }),
+  }));
 
-export const getServicesLevel0 = (lang: string) => {
   const svc = lang === "en" ? en0 : no0;
   return shuffle(svc);
 };
